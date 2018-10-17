22581359466c1fde983060492a29c0a1d18179d2
PickleJr <picklejr.t.gonzalez@gmail.com>
2018-10-17 15:28:54 -0500

    Initial setup of manifest and service worker - testing phase

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendfile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
