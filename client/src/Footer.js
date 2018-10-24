663dca2e48808a7050093f6d558e2ffd0b0e47ff
Taylor Gonzalez <taylordgonzalez@outlook.com>
2018-10-23 19:47:42 -0500

    Implemented toggler that might remove footer when keyboard is up

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -7,9 +7,49 @@ import { NavLink } from 'react-router-dom';
 import './Footer.css';
 
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        let isUpright = window.orientation === 0 || window.orientation === 180;
+        this.state = {
+            style: {
+                display: 'block'
+            },
+            portraitScreenHeight: (isUpright) ? window.innerHeight : window.innerWidth,
+            landscapeScreenHeight: (isUpright) ? window.innerWidth : window.innerHeight
+        };
+        this.tolerance = 25;
+        this.footerToggler = this.footerToggler.bind(this);
+    }
+
+    footerToggler() {
+        let isUpright = window.orientation === 0 || window.orientation === 180;
+        let newState = this.state;
+        if(isUpright && ((window.innerHeight + this.tolerance) < this.state.portraitScreenHeight)) {
+            // keyboard visible in portrait
+            newState.style.display = 'none';
+        } else if((window.innerHeight + this.tolerance) < this.state.landscapeScreenHeight) {
+            // keyboard visible in landscape
+            newState.style.display = 'none';
+        } else {
+            // keyboard NOT visible
+            newState.style.display = 'block';
+        }
+        this.setState(newState);
+    }
+
+    componentDidMount() {
+        let isMobile = /Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        if(isMobile) window.addEventListener('resize', this.footerToggler);
+    }
+
+    componentWillUnmount() {
+        let isMobile = /Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        if(isMobile) window.removeEventListener('resize', this.footerToggler);
+    }
+
     render() {
         return (
-            <footer>
+            <footer style={this.state.style}>
                 <div className="container">
                     <div id="b-nav" className="row center">
                         <NavLink exact to='/' className="nav col s6">
